0fffd8b3fcceaf6ed967b94570bf63844dd16225
Mattthew Pourroy <69867050+pourroymatt750@users.noreply.github.com>
2025-05-12 22:50:53 -0400

    run migrations from code

--- watchbox_mtp/wsgi.py
+++ watchbox_mtp/wsgi.py
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watchbox_mtp.settings')
 
+# Run migrations automatically
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migration failed: {e}")
+
 application = get_wsgi_application()
